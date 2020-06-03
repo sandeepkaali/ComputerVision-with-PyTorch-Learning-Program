303361565b3a48049f20aff094067ef0d10914d9
Sandeep K <blackgen629@gmail.com>
2020-06-03 20:54:45 +0530

    Update main.py

--- Tasks/registration_task/main.py
+++ Tasks/registration_task/main.py
@@ -7,15 +7,24 @@ y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 import numpy
 
-class LinearRegression(object):
-  """
-  An implementation of linear regression model
-  """
+class LinearRegression:
+    def fit(self,X,Y):
+      X=np.array(X).reshape(-1,1)
+      Y=np.array(Y).reshape(-1,1)
+      
+      x_shape = X.shape
+      
+      num_var = x_shape[1]
+      weight_matrix = np.random.normal(0,1,(num_var,1))
+      intercept = np.random.rand(1)
+      for i in range(50):
+          dcostdm = np.sum(np.multiply(((np.matmul(X,weight_matrix)+intercept)-Y),X))*2/x_shape[0]
+          dcostdc = np.sum(((np.matmul(X,weight_matrix)+intercept)-Y))*2/x_shape[0]
+          weight_matrix -= 0.1*dcostdm
+          intercept -= 0.1*dcostdc
+      return weight_matrix,intercept
   
-  def fit(_input, _output):
-    pass
-    
-  def predict(_input):
-    pass
-    
+    def predict(self, X):
+      product = np.matmul(np.array(X).reshape(-1,1),self.weight_matrix)+self.intercept
+      return product
     
